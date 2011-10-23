9d2f91089dadbe795a16da9d84cb0da33cea9a37
Pepijn de Vos <pepijndevos@gmail.com>
2011-10-23 11:45:12 +0200

    Welcome to this gitorial. I just created an empty Leiningen project with `lein new wiki` and initiated git with `git init`

---
+++ src/wiki/core.clj
@@ -0,0 +1,1 @@
+(ns wiki.core)
---
+++ test/wiki/test/core.clj
@@ -0,0 +1,6 @@
+(ns wiki.test.core
+  (:use [wiki.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
